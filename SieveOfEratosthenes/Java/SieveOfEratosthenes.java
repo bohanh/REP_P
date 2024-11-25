00456b52f221643c7341c7bbf2060896d3198a0f
chenyu <chenyu.Li@insa-rennes.fr>
2024-11-25 10:01:53 +0100

    java soe update

--- SieveOfEratosthenes/Java/SieveOfEratosthenes.java
+++ SieveOfEratosthenes/Java/SieveOfEratosthenes.java
@@ -1,22 +1,36 @@
 import java.util.LinkedList;
 
-public class SieveOfEratosthenes{
-       public static LinkedList<Integer> sieve(int n){
-               if(n < 2) return new LinkedList<Integer>();
-               LinkedList<Integer> primes = new LinkedList<Integer>();
-               LinkedList<Integer> nums = new LinkedList<Integer>();
+public class SieveOfEratosthenes {
+        // Method to find primes using the Sieve of Eratosthenes
+        public static LinkedList<Integer> sieve(int n) {
+                if (n < 2) return new LinkedList<Integer>();
 
-               for(int i = 2;i <= n;i++){ //unoptimized
-                       nums.add(i);
-               }
+                LinkedList<Integer> primes = new LinkedList<Integer>();
+                LinkedList<Integer> nums = new LinkedList<Integer>();
 
-               while(nums.size() > 0){
-                       int nextPrime = nums.remove();
-                       for(int i = nextPrime * nextPrime;i <= n;i += nextPrime){
-                               nums.removeFirstOccurrence(i);
-                       }
-                       primes.add(nextPrime);
-               }
-               return primes;
-       }
+                // Add all numbers from 2 to n
+                for (int i = 2; i <= n; i++) {
+                        nums.add(i);
+                }
+
+                // Sieve algorithm: remove non-primes
+                while (nums.size() > 0) {
+                        int nextPrime = nums.remove(); // The first number is always prime
+                        for (int i = nextPrime * nextPrime; i <= n; i += nextPrime) {
+                                nums.removeFirstOccurrence(i); // Remove multiples of nextPrime
+                        }
+                        primes.add(nextPrime); // Add prime to the result list
+                }
+                return primes;
+        }
+
+        // Main method to test the sieve
+        public static void main(String[] args) {
+                // Test the sieve function
+                int n = 50; // Find all primes up to 50
+                LinkedList<Integer> primes = sieve(n);
+
+                // Print the primes
+                System.out.println("Primes up to " + n + ": " + primes);
+        }
 }
